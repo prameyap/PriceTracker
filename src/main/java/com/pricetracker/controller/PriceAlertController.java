ec58c6f14a3792f931b8541d2afe76873e2e1de7
prameyap <pprameya93@gmail.com>
2025-08-07 09:13:06 +0100

    updated logger

--- src/main/java/com/pricetracker/controller/PriceAlertController.java
+++ src/main/java/com/pricetracker/controller/PriceAlertController.java
@@ -4,17 +4,18 @@ import com.pricetracker.requestmodel.PriceAlertRequest;
 import com.pricetracker.responsemodel.PriceAlertResponse;
 import com.pricetracker.service.PriceAlertService;
 import jakarta.validation.Valid;
-import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 
-@Slf4j
 @RestController
 @RequestMapping("/api/price-alerts")
 public class PriceAlertController {
+    private static final Logger log = LoggerFactory.getLogger(PriceAlertController.class);
     @Autowired
     private PriceAlertService priceAlertService;
 
